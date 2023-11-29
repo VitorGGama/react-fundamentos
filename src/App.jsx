dea3c31d00dd03139a82df21a6e021ab4014edbd
vitor.gama <vitorggam@hotmail.com>
2023-11-29 11:10:49 -0300

    formatacao com prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 function App() {
-  return <>
-    <header>
-    <h1>Olá React!😆</h1>
-  </header>
-
-  <main>
-    <h2>Teste</h2>
-  </main>
- </>
+  // Comentário de uma linha CTRL + ,
+  /* Comentários JS de várias linhas */
+  return (
+    <>
+      <header>
+        <h1>Olá React!😆</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
+      {/* Comentário no meio de JSX
+    é normal ficar entre{} nesta situação. */}
+      <main>
+        <h2>Teste</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
