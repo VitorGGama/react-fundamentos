328760b9358f18a6fcb5a7f502fb27b831b340c4
vitor.gama <vitorggam@hotmail.com>
2023-11-30 10:00:37 -0300

    css global

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-/* Importação das libs ESSENCIAIS (React e ReactDOM) */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-/*Importação do primeiro/principal componente,
-arquivo em que carregamos os elementos da página (como
-  se fosse um index/home) */
-import App from './App.jsx'
- /*Inicialização dos recursos do React, manipundo a div#root
- existente no index.html. */ 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/* Importação dos estilos globais*/
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
