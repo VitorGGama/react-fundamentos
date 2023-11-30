370b1084f5eac03c7d37096b7c999ecaf6113bda
vitor.gama <vitorggam@hotmail.com>
2023-11-30 11:34:38 -0300

    classes combinadas

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -12,6 +12,9 @@ function Cabecalho() {
         <span className={estilos.sombra}>React!</span>😆
       </h1>
       <hr />
+      <p className={`${estilos["destaque-texto"]} centralizar`}>
+        Classes combinadas
+      </p>
       <Menu />
     </header>
   );
