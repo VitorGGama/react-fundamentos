cc4ac483f83ea32d536bca9348b525d3d2ef1fc3
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-01-15 11:09:50 -0300

    comentario

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function Produtos() {
+  /*O state "produtos" é iniciado como um array vazio.
+  Posteriormente (após o carregamento dos dados da API),
+  ele será preenchido com os objetos/produtos. */
+  const [produtos, setProdutos] = useState();
 
   const carregarDados = async () => {
     try {
       const resposta = await fetch(`https://fakestoreapi.com/products`);
       const dados = await resposta.json();
+      console.log(dados);
+      setProdutos(dados);
       console.log(dados);
     } catch (error) {
       console.error("Houve um erro: "+error);
